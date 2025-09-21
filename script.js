c9e3200e75c84839d6252548ed1a5c9e766c2a57
Amol Subhash Tayade <amoltayade459@gmail.com>
2025-09-21 17:43:53 +0530

    script.js

--- script.js
+++ script.js
@@ -39,45 +39,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }, { threshold: 0.2 });
   elements.forEach(el => observer.observe(el));
 });
-// const sr = ScrollReveal({
-    //   origin:'top',
-    //   distance :'80px',
-    //   duration: 2000,
-    //   reset: true   // smoother UX
-    // });
-    
-    // sr.reveal('h2',{})
-    // sr.reveal('#home p',{})
-    // sr.reveal('#home .download-btn', { delay: 100, reset: true });
-    // sr.reveal('#home .logo',{interval:100})
-    // sr.reveal('#home .profile',{delay:400})
-    // sr.reveal('dotlottie-wc',{delay:200})
-    
-    // const paragraphs = document.querySelectorAll('.about-text p');
-    // paragraphs.forEach((el, index) => {
-    //   ScrollReveal().reveal(el,{ delay: 200 * (index + 1) });
-    // });
-    
-    // const skills = document.querySelectorAll('.skill-card');
-    // skills.forEach((el,index)=>{
-    //   ScrollReveal().reveal(el,{ delay: 200 * (index + 1) });
-    // });
-    
-    // const projects = document.querySelectorAll('.project-card');
-    // sr.reveal('#skills h2',{delay:200*(projects.length)})
-    // projects.forEach((el, index) => {
-    //   ScrollReveal().reveal(el,{ delay: 200 * (index + 1) });
-    // });
-    
-    // sr.reveal('#contact h2',{delay:200})
-    // sr.reveal('.inp1',{delay:200})
-    // sr.reveal('.inp2',{delay:400})
-    // sr.reveal('.inp3',{delay:600})
-    // sr.reveal('button',{delay:800})
-    
-    // const menuToggle = document.querySelector(".menu-toggle");
-    // const navLinks = document.querySelector("nav ul");
     
     // menuToggle.addEventListener("click", () => {
     //   navLinks.classList.toggle("active");
+
     // });
